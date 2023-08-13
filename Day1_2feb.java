197601da94ae697016a129ea5fddfa4736e2ee4a
Khushboo Yadav <112149305+Khushboo1013@users.noreply.github.com>
2023-08-13 17:09:58 +0530

    Java code .Java
    New code for test

---
+++ Day1_2feb.java
@@ -0,0 +1,8 @@
+public class Day1_2feb {
+    public static void main(String[] args) {
+        System.out.print("Hello World"); //give output
+        System.out.println("My Learning");
+        System.out.print("My name is Khushboo\n");
+    }
+}
+// boilerplate code
